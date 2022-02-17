7f5bf1f6527169ac9590c2910a1fd63484d681a7
Shinichi Urano <s@urano.me>
2022-02-17 15:05:24 -0500

    allow future versions of terraform providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.15.0"
+      version = ">= 2.15.0"
     }
   }
 }
